0214d9b4fbe1b2ab7e131943e863b745f9877090
geckobees <98201133+geckobees@users.noreply.github.com>
2023-10-11 12:23:28 -0400

    new stuff

--- app/src/main/java/com/example/fitnessapp/MainActivity.kt
+++ app/src/main/java/com/example/fitnessapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.fitnessapp
 
+import android.app.Application
 import android.os.Bundle
+import android.provider.ContactsContract.CommonDataKinds.Email
 import android.widget.Button
+import android.widget.EditText
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,7 +14,18 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.room.ColumnInfo
+import androidx.room.Dao
+import androidx.room.Database
+import androidx.room.Delete
+import androidx.room.Entity
+import androidx.room.Insert
+import androidx.room.PrimaryKey
+import androidx.room.Query
+import androidx.room.RoomDatabase
 import com.example.fitnessapp.ui.theme.FitnessAppTheme
+import kotlinx.coroutines.selects.select
+
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,9 +33,38 @@ class MainActivity : ComponentActivity() {
         setContentView(R.layout.layout)
 
         val button = findViewById<Button>(R.id.button_id)
+        val username = findViewById<EditText>(R.id.username)
+        val signup = findViewById<Button>(R.id.signup)
 
-        button.setOnClickListener(){
+        signup.setOnClickListener(){
 
         }
     }
 }
+
+
+@Entity
+data class User(
+    @PrimaryKey val uuid: Int,
+    @ColumnInfo(name = "user_name") val username: String?,
+    @ColumnInfo(name = "password") val password: String?
+
+)
+@Dao
+interface UserAccess {
+    @Insert
+    fun insertAll(vararg users: User)
+
+    @Delete
+    fun delete(user: User)
+
+    @Query("SELECT * FROM user")
+    fun search(user : User)
+}
+@Database(entities = [User :: class], version = 1)
+abstract class AppDB : RoomDatabase() {
+    abstract fun userDao(): UserAccess
+}
+
+
+
